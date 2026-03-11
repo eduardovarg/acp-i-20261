422e78d941dbc3a9556de2a1573d75c1e06c5af2
unknown <2612082028@ACADEMICO.IESB.BR>
2026-03-11 10:59:08 -0300

    adicinar codigos aula02

---
+++ aulas/aula02/saida.c
@@ -0,0 +1,14 @@
+#include <stdio.h> // inclusao de bibliotecas da  linguagem
+
+int main() {
+    //1. Entrada 
+    printf("acordei e levantei\n");
+    //2. Processamento
+    printf("aribaba\n");
+    //3. Saida
+    printf("to com fome\n");
+    printf("vou comer uma feijoada\n");
+    printf("%i\n", 10);
+    printf("%f\n" , 10.345500);
+   return 0;
+}
